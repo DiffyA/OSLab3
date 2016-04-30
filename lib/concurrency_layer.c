b1071536c03459aab5eb840c8abeab71adb47bae
DiffyA <vdegou@gmail.com>
2016-05-01 01:08:55 +0200

    Update concurrency_layer.c
    Fixed concurrency issues between broker and executer threads.

--- lib/concurrency_layer.c
+++ lib/concurrency_layer.c
@@ -6,11 +6,13 @@
 pthread_mutex_t fullQueueMutex;
 pthread_mutex_t globalWrite;
 pthread_cond_t fullQueue;
+pthread_cond_t emptyQueue;
 
 void init_concurrency_mechanisms() {
     pthread_mutex_init(&fullQueueMutex, NULL);
     pthread_mutex_init(&globalWrite, NULL);
     pthread_cond_init(&fullQueue, NULL);
+    pthread_cond_init(&emptyQueue, NULL);
 }
 
 void destroy_concurrency_mechanisms() {
@@ -18,16 +20,10 @@ void destroy_concurrency_mechanisms() {
 
 // Broker: inserts operations obtained from a batch file into the operations_queue of a given market.
 void * broker(void * args) {
-/*Input: broker_info --> 
+/*Input: broker_info —> 
 1) char batch_file[256]
 2) stock_market * market)
 */
-/*    pthread_mutex_lock(&fullQueueMutex);
-    printf("ENTER BROKER \n");
-    uleep(500000);
-    pthread_mutex_unlock(&full_queue_mutex);
-*/
-	usleep(5000);
     broker_info * currentInfo = (broker_info*) args; // Cast the *void pointer to a *broker_info pointer. 
     stock_market * currentMarket = currentInfo->market; // Create a stock_market pointer. This might not be necessary but handy in the enqueue_operation call.
     operation currentOperation; // operation struct to hold information of the operation on current batch line.
@@ -35,53 +31,58 @@ void * broker(void * args) {
 
     if (brokerIterator == NULL) { // Error creating a new iterator. 
         perror("Error creating a new iterator. \n"); 
-        return; // void calls return no value. This might show up as a warning however.
+        pthread_exit(NULL);
     }
     else { // Iterator was created succesfully
-    	while (next_operation(brokerIterator, currentOperation.id, &currentOperation.type, &currentOperation.num_shares, &currentOperation.share_price) != EOF) {  // While there are still lines to read, continue.
-		pthread_mutex_lock(&globalWrite);
-		enqueue_operation(currentMarket->stock_operations, &currentOperation); // Enqueues the operation created by the next_operation call.
-		printf("CURRENT ID: %d \n", pthread_self());
-		usleep(500000);
-		while (operations_queue_full(currentMarket->stock_operations) == 1) {
-			printf("QUEUE FULL! WAITING! \n");
-			pthread_cond_wait(&fullQueue, &globalWrite);
-		}
-		pthread_mutex_unlock(&globalWrite);
-		usleep(100000);
-	}
-	
-	destroy_iterator(brokerIterator); // Destroys iterator to free resources.
+        pthread_mutex_lock(&globalWrite);
+        while (next_operation(brokerIterator, currentOperation.id, &currentOperation.type, &currentOperation.num_shares, &currentOperation.share_price) != EOF) {  // While there are still lines to read, continue.
+            while (operations_queue_full(currentMarket->stock_operations) == 1) { // When the queue is full
+                pthread_cond_wait(&fullQueue, &globalWrite); // Wait until you get the signal it is no longer full. 
+            }
+            enqueue_operation(currentMarket->stock_operations, &currentOperation); // Enqueues the operation created by the next_operation call.
+            pthread_cond_signal(&emptyQueue); // Signal to the executer that the queue is no longer empty.
+        }
+        pthread_mutex_unlock(&globalWrite);
+        destroy_iterator(brokerIterator); // Destroys iterator to free resources.
     }
+    pthread_exit(NULL);
 }
 
 
 void* operation_executer(void * args) {
-/* Input: exec_info -->
+/* Input: exec_info —>
 1) int *exit
 2) stock_market * market
 3) pthread_mutex_t * exit_mutex
 */
-    printf("ENTER OP_EXEC \n");
     exec_info * currentInfo = (exec_info*) args;
     stock_market * currentMarket = currentInfo->market;
     operation currentOperation;
 
-    while (*(currentInfo->exit) != 1) { // While exit flag is not active
-	pthread_mutex_lock(&globalWrite);
-	if (operations_queue_empty(currentMarket->stock_operations) == 0) { // If queue is not empty
-	    dequeue_operation(currentMarket->stock_operations , &currentOperation);
-	    pthread_cond_signal(&fullQueue); // Queue is no longer full as one operation has been dequeued.
-	    process_operation(currentInfo->market, &currentOperation);
-	}
-
-	pthread_mutex_unlock(&globalWrite);
+    while(1){ // Will do continuously until no more operations in the queue and exit flag is on.
+        pthread_mutex_lock(&globalWrite); // We will access and modify the queue, so lock it.
+        if (operations_queue_empty(currentMarket->stock_operations) == 0) { // If queue is not empty
+            dequeue_operation(currentMarket->stock_operations , &currentOperation); // Dequeue the operation.
+            pthread_cond_signal(&fullQueue); // Send signal that queue is no longer full as one operation has been dequeued.
+            process_operation(currentInfo->market, &currentOperation); // Process the dequeued operation.
+        }
+        else { //Queue is empty
+            pthread_mutex_lock((currentInfo->exit_mutex)); // Lock the exit mutex as we are about to access the exit variable.
+            if (*(currentInfo->exit)) { // Exit is active: we are not waiting for more brokers.
+                pthread_mutex_unlock((currentInfo->exit_mutex)); // Unlock exit mutex.
+                pthread_exit(NULL); // Thread exits.
+            }
+            else { // If exit is not active: we are waiting for more brokers' operations.
+                pthread_cond_wait(&emptyQueue, &globalWrite); // Wait for a signal from the variable, unlock the globalWrite so a broker may use CPU                                
+            }
+            pthread_mutex_unlock((currentInfo->exit_mutex));
+        }
+        pthread_mutex_unlock(&globalWrite); // Unlock the globalWrite.
     }
-    return;
 }
 
 void * stats_reader(void * args){ 
-/*Input: reader_info --> 
+/*Input: reader_info —> 
 1) int * exit
 2) stock_market * market
 3) pthread_mutex_t * exit_mutex
