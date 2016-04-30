6746c534d43ab42ccd33ffc326845767afc876b3
DiffyA <vdegou@gmail.com>
2016-04-30 21:14:23 +0200

    Update concurrency_layer.c
    Added concurrency mechanisms between broker and executer.

--- lib/concurrency_layer.c
+++ lib/concurrency_layer.c
@@ -3,7 +3,14 @@
 #include <unistd.h>
 #include "../include/concurrency_layer.h"
 
+pthread_mutex_t fullQueueMutex;
+pthread_mutex_t globalWrite;
+pthread_cond_t fullQueue;
+
 void init_concurrency_mechanisms() {
+    pthread_mutex_init(&fullQueueMutex, NULL);
+    pthread_mutex_init(&globalWrite, NULL);
+    pthread_cond_init(&fullQueue, NULL);
 }
 
 void destroy_concurrency_mechanisms() {
@@ -15,6 +22,12 @@ void * broker(void * args) {
 1) char batch_file[256]
 2) stock_market * market)
 */
+/*    pthread_mutex_lock(&fullQueueMutex);
+    printf("ENTER BROKER \n");
+    uleep(500000);
+    pthread_mutex_unlock(&full_queue_mutex);
+*/
+	usleep(5000);
     broker_info * currentInfo = (broker_info*) args; // Cast the *void pointer to a *broker_info pointer. 
     stock_market * currentMarket = currentInfo->market; // Create a stock_market pointer. This might not be necessary but handy in the enqueue_operation call.
     operation currentOperation; // operation struct to hold information of the operation on current batch line.
@@ -26,32 +39,60 @@ void * broker(void * args) {
     }
     else { // Iterator was created succesfully
     	while (next_operation(brokerIterator, currentOperation.id, &currentOperation.type, &currentOperation.num_shares, &currentOperation.share_price) != EOF) {  // While there are still lines to read, continue.
+		pthread_mutex_lock(&globalWrite);
 		enqueue_operation(currentMarket->stock_operations, &currentOperation); // Enqueues the operation created by the next_operation call.
+		printf("CURRENT ID: %d \n", pthread_self());
+		usleep(500000);
+		while (operations_queue_full(currentMarket->stock_operations) == 1) {
+			printf("QUEUE FULL! WAITING! \n");
+			pthread_cond_wait(&fullQueue, &globalWrite);
+		}
+		pthread_mutex_unlock(&globalWrite);
+		usleep(100000);
 	}
-    
+	
 	destroy_iterator(brokerIterator); // Destroys iterator to free resources.
     }
 }
 
-// operation_executer: retrieves operation from the operations_queue and processes them, causing an update in the market. 
+
 void* operation_executer(void * args) {
 /* Input: exec_info -->
 1) int *exit
 2) stock_market * market
 3) pthread_mutex_t * exit_mutex
 */
-    exec_info * currentInfo = (exec_info*) args; // Cast the *void pointer to a *exec_info pointer.
-    stock_market * currentMarket = currentInfo->market; // Create a stock_market pointer by dereferencing from the currentInfo pointer.
-    operation currentOperation; // operation struct to hold information of the last operation extracted from the queue.
+    printf("ENTER OP_EXEC \n");
+    exec_info * currentInfo = (exec_info*) args;
+    stock_market * currentMarket = currentInfo->market;
+    operation currentOperation;
 
-    while (*(currentInfo->exit) != 1) { // While exit flag is not active.
-	   if (operations_queue_empty(currentMarket->stock_operations) == 0) { // If operations_queue is not empty.
-	       dequeue_operation(currentMarket->stock_operations , &currentOperation); // Obtain the first operation.
-	       process_operation(currentInfo->market, &currentOperation); // Process it.
-	   }
+    while (*(currentInfo->exit) != 1) { // While exit flag is not active
+	pthread_mutex_lock(&globalWrite);
+	if (operations_queue_empty(currentMarket->stock_operations) == 0) { // If queue is not empty
+	    dequeue_operation(currentMarket->stock_operations , &currentOperation);
+	    pthread_cond_signal(&fullQueue); // Queue is no longer full as one operation has been dequeued.
+	    process_operation(currentInfo->market, &currentOperation);
+	}
+
+	pthread_mutex_unlock(&globalWrite);
     }
     return;
 }
 
-void* stats_reader(void * args) {
+void * stats_reader(void * args){ 
+/*Input: reader_info --> 
+1) int * exit
+2) stock_market * market
+3) pthread_mutex_t * exit_mutex
+4) unsigned int frequency
+*/
+      printf("STAT READER \n");
+      reader_info * currentInfo  = (reader_info*) args;
+      stock_market * currentMarket = currentInfo->market;
+
+     // while(*(currentInfo->exit) != 1){
+
+      update_market_statistics(currentInfo->market);
+      usleep(currentInfo->frequency);
 }
