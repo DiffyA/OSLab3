983c7a94d04bea217abc474572fb71e751694030
fedede <fedede@users.noreply.github.com>
2016-05-02 18:41:45 +0200

    concurrency working for 2+ readers

--- lib/concurrency_layer.c
+++ lib/concurrency_layer.c
@@ -8,11 +8,18 @@ pthread_mutex_t globalWrite;
 pthread_cond_t fullQueue;
 pthread_cond_t emptyQueue;
 
+pthread_mutex_t numReadersMutex;
+
+int numReaders;
+
 void init_concurrency_mechanisms() {
     pthread_mutex_init(&fullQueueMutex, NULL);
     pthread_mutex_init(&globalWrite, NULL);
     pthread_cond_init(&fullQueue, NULL);
     pthread_cond_init(&emptyQueue, NULL);
+    
+    numReaders = 0;
+    pthread_mutex_init(&numReadersMutex, NULL);
 }
 
 void destroy_concurrency_mechanisms() {
@@ -20,7 +27,7 @@ void destroy_concurrency_mechanisms() {
 
 // Broker: inserts operations obtained from a batch file into the operations_queue of a given market.
 void * broker(void * args) {
-/*Input: broker_info —> 
+/*Input: broker_info �> 
 1) char batch_file[256]
 2) stock_market * market)
 */
@@ -34,15 +41,15 @@ void * broker(void * args) {
         pthread_exit(NULL);
     }
     else { // Iterator was created succesfully
-        pthread_mutex_lock(&globalWrite);
         while (next_operation(brokerIterator, currentOperation.id, &currentOperation.type, &currentOperation.num_shares, &currentOperation.share_price) != EOF) {  // While there are still lines to read, continue.
+            pthread_mutex_lock(&globalWrite);
             while (operations_queue_full(currentMarket->stock_operations) == 1) { // When the queue is full
                 pthread_cond_wait(&fullQueue, &globalWrite); // Wait until you get the signal it is no longer full. 
             }
             enqueue_operation(currentMarket->stock_operations, &currentOperation); // Enqueues the operation created by the next_operation call.
             pthread_cond_signal(&emptyQueue); // Signal to the executer that the queue is no longer empty.
+            pthread_mutex_unlock(&globalWrite);
         }
-        pthread_mutex_unlock(&globalWrite);
         destroy_iterator(brokerIterator); // Destroys iterator to free resources.
     }
     pthread_exit(NULL);
@@ -50,7 +57,7 @@ void * broker(void * args) {
 
 
 void* operation_executer(void * args) {
-/* Input: exec_info —>
+/* Input: exec_info �>
 1) int *exit
 2) stock_market * market
 3) pthread_mutex_t * exit_mutex
@@ -70,30 +77,76 @@ void* operation_executer(void * args) {
             pthread_mutex_lock((currentInfo->exit_mutex)); // Lock the exit mutex as we are about to access the exit variable.
             if (*(currentInfo->exit)) { // Exit is active: we are not waiting for more brokers.
                 pthread_mutex_unlock((currentInfo->exit_mutex)); // Unlock exit mutex.
+                pthread_mutex_unlock(&globalWrite); // Unlock the globalWrite.
                 pthread_exit(NULL); // Thread exits.
             }
             else { // If exit is not active: we are waiting for more brokers' operations.
-                pthread_cond_wait(&emptyQueue, &globalWrite); // Wait for a signal from the variable, unlock the globalWrite so a broker may use CPU                                
+                pthread_mutex_unlock((currentInfo->exit_mutex));
+                pthread_cond_wait(&emptyQueue, &globalWrite); // Wait for a signal from the variable, unlock the globalWrite so a broker may use CPU 
+                                        
             }
-            pthread_mutex_unlock((currentInfo->exit_mutex));
+           
         }
-        pthread_mutex_unlock(&globalWrite); // Unlock the globalWrite.
+        pthread_mutex_unlock(&globalWrite); // Unlock the globalWrite.       
     }
 }
 
 void * stats_reader(void * args){ 
-/*Input: reader_info —> 
+/*Input: reader_info �> 
 1) int * exit
 2) stock_market * market
 3) pthread_mutex_t * exit_mutex
 4) unsigned int frequency
 */
+
       printf("STAT READER \n");
       reader_info * currentInfo  = (reader_info*) args;
       stock_market * currentMarket = currentInfo->market;
-
-     // while(*(currentInfo->exit) != 1){
-
-      update_market_statistics(currentInfo->market);
+      
+      pthread_mutex_lock((currentInfo->exit_mutex));  
+      while(*(currentInfo->exit) != 1){
+      pthread_mutex_unlock((currentInfo->exit_mutex));
+      
+      pthread_mutex_lock(&numReadersMutex);//bloqueamos numReaders
+      
+      if(numReaders==0){//Es el primero en llegar
+      numReaders++; 
+      pthread_mutex_lock(&globalWrite); //No puede pasar de aqui hasta que no haya ni broker ni exec
+      pthread_mutex_unlock(&numReadersMutex); //Esto se abre aqui para que el resto de readers se bloqueen en la linea 107 y no puedan pasar mientras este siga bloqueado por globalWrite
+      
+      print_market_status(currentMarket);
+      
+      pthread_mutex_lock(&numReadersMutex);
+      numReaders--;
+      pthread_mutex_unlock(&numReadersMutex);
+     
+      }else{//Hasta aqui es lo que haria el primero que llegue (es solamente porque luego se hace numReaders++ para los otros y si no el "padre" lo haria dos veces
+      //Aqui se entiende mas o menos lo que hacen
+      //es igual que antes. Si te das cuenta el globalWrite sigue bloqueado
+      
+      numReaders++;
+      pthread_mutex_unlock(&numReadersMutex);
+      
+      print_market_status(currentMarket);
+      
+      pthread_mutex_lock(&numReadersMutex);
+      numReaders--;
+      pthread_mutex_unlock(&numReadersMutex);
+      }
+      
+      pthread_mutex_lock(&numReadersMutex);
+      if(numReaders == 0){ //Aqui entraria solamente el ultimo, que es cuando no tendriamos mas readers haciendo cosas.
+            pthread_mutex_unlock(&globalWrite); //Esto no se si importaria el orden y si primero abrir el global o primero el numreaders pero basicamente abre ese mutex antes de hacer el sleep
+                                             //Despues de esto puede que legue otro reader con el mismo proceso de antes o puede que entre uno de los otros. Ypunto
+            pthread_mutex_unlock(&numReadersMutex);
+      }else{
+            pthread_mutex_unlock(&numReadersMutex); 
+      }
+      
       usleep(currentInfo->frequency);
+      pthread_mutex_lock((currentInfo->exit_mutex)); 
+}
+      pthread_mutex_unlock((currentInfo->exit_mutex));
+      pthread_exit(NULL);
+
 }
